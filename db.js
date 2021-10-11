b182204a83f410411a5234278f9a5b6954d5c261
Ajay Bharat Kolla <ajaybharat222@gmail.com>
2021-10-11 17:41:04 +0530

    updated mogodb atlas connection string

--- db.js
+++ db.js
@@ -1,11 +1,13 @@
 const mongoose = require("mongoose");
-// require("dotenv").config();
+require("dotenv").config();
 
 mongoose
-  .connect("mongodb://localhost/users", {
+  .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     // useCreateIndex: true,
     useUnifiedTopology: true,
   })
   .then(() => console.log("Database connection successfull"))
   .catch((error) => console.log(`Database connection error ${error}`));
+
+  // mongoose.connect(mongodb+srv://clusterAnything.mongodb.net/test?retryWrites=true&w=majority, { user: process.env.MONGO_USER, pass: process.env.MONGO_PASSWORD, useNewUrlParser: true, useUnifiedTopology: true });
